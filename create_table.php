3e2f70a1fe6084337a302ccff5dbbfa13ffc0b4e
mllemango <msmangochan@gmail.com>
2018-11-21 18:53:14 -0500

    why oh why

--- create_table.php
+++ create_table.php
@@ -5,13 +5,13 @@ $sql = "CREATE TABLE goose
     id SERIAL PRIMARY KEY,
     position VARCHAR NOT NULL,
     category VARCHAR NOT NULL,
-    g_location VARCHAR,
     created_on TIMESTAMP NOT NULL,
    )";
 $result=pg_query($conn,$sql);
 if(!$result) { 
     $error= pg_last_error($conn);
     echo $error;
+    echo 'why oh why';
 }
 else{
     echo 'success';
