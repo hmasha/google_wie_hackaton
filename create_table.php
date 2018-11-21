2074ee3ca515b5374474c3a992db29c0ef9e63fe
mllemango <msmangochan@gmail.com>
2018-11-21 18:42:44 -0500

    psql create statement change

--- create_table.php
+++ create_table.php
@@ -1,12 +1,13 @@
 <?php
 include 'connect.php';
-$sql = 'CREATE TABLE goose(
-    user_id serial PRIMARY KEY,
-    position VARCHAR (50) UNIQUE NOT NULL,
-    category VARCHAR (50) NOT NULL,
-    location VARCHAR (355) NULL,
+$sql = "CREATE TABLE public.goose
+(
+    user_id SERIAL PRIMARY KEY,
+    position VARCHAR UNIQUE NOT NULL,
+    category VARCHAR NOT NULL,
+    g_location VARCHAR NULL,
     created_on TIMESTAMP NOT NULL,
-   );';
+   )";
 $result=pg_query($conn,$sql);
 if(!$result) { 
     $error= pg_last_error($conn);
