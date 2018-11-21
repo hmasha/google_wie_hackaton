0867177a73e1cc76d6a699c2a276b923f6e3bb4c
mllemango <msmangochan@gmail.com>
2018-11-21 18:55:49 -0500

    updated sql

--- create_table.php
+++ create_table.php
@@ -5,7 +5,7 @@ $sql = "CREATE TABLE goose
     id SERIAL PRIMARY KEY,
     position VARCHAR NOT NULL,
     category VARCHAR NOT NULL,
-    created_on TIMESTAMP NOT NULL,
+    created_on TIMESTAMP NOT NULL
    )";
 $result=pg_query($conn,$sql);
 if(!$result) { 
