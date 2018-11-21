4c842e3de9716a0880d43665d6a88d76df69645c
mllemango <msmangochan@gmail.com>
2018-11-21 18:44:20 -0500

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -5,9 +5,9 @@ $sql = "CREATE TABLE public.goose
     user_id SERIAL PRIMARY KEY,
     position VARCHAR UNIQUE NOT NULL,
     category VARCHAR NOT NULL,
-    g_location VARCHAR NULL,
+    g_location VARCHAR,
     created_on TIMESTAMP NOT NULL,
-   )";
+   );";
 $result=pg_query($conn,$sql);
 if(!$result) { 
     $error= pg_last_error($conn);
