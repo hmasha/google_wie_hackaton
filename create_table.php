300a2c71f3ca5a0817779519cce39b8ed4ad1e16
mllemango <msmangochan@gmail.com>
2018-11-21 18:50:52 -0500

    update sql in create_table

--- create_table.php
+++ create_table.php
@@ -1,13 +1,13 @@
 <?php
 include 'connect.php';
-$sql = "CREATE TABLE public.goose
+$sql = "CREATE TABLE goose
 (
-    user_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     position VARCHAR NOT NULL,
     category VARCHAR NOT NULL,
     g_location VARCHAR,
     created_on TIMESTAMP NOT NULL,
-   );";
+   )";
 $result=pg_query($conn,$sql);
 if(!$result) { 
     $error= pg_last_error($conn);
