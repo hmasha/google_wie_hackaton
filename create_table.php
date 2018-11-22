0c0b6d6a1215e36cc563cfe8a9dc26dcc83e6c9b
mllemango <msmangochan@gmail.com>
2018-11-21 19:04:37 -0500

    lat and longupdate

--- create_table.php
+++ create_table.php
@@ -1,10 +1,14 @@
 <?php
 include 'connect.php';
+$drop ="DROP TABLE goose";
+$resultdrop=pg_query($conn,$drop);
 $sql = "CREATE TABLE goose
 (
     id SERIAL PRIMARY KEY,
     position VARCHAR NOT NULL,
-    category VARCHAR NOT NULL,
+    lat VARCHAR NOT NULL,
+    long VARCHAR NOT NULL,
+    position VARCHAR,
     created_on TIMESTAMP NOT NULL
    )";
 $result=pg_query($conn,$sql);
