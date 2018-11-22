e513f881f5d2630fb098dbbd7713b3d50aff8305
Melanie Wang <msmangochan@gmail.com>
2018-11-21 19:30:39 -0500

    hidden form to submit location

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
       #map {
-        height: 100%;
+        height: 80%;
+        width: 80%;
       }
       /* Optional: Makes the sample page fill the window. */
       html, body {
@@ -22,10 +23,15 @@
   <body>
   
     <div id="map"></div>
+    <form action="/insert_data.php" method="post" id="form1">
+      <input type="hidden" id="myLat" name="lat">
+      <input type="hidden" id="myLong" name="long">
+      <input type="submit" value="IM NEAR A GOOSE">
+    </form>
     <script>
       //this code is from https://developers.google.com/maps/documentation/javascript/geolocation
       // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
+      // prompted by your browser.                     If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
       var map, infoWindow;
@@ -43,17 +49,22 @@
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
+            
+            //setting the hidden form variables
+            document.getElementById('myLat').value = position.coords.latitude;
+            document.getElementById('myLong').value = position.coords.longitude;
+            //test postition for a marker
             var pos2 = {
               lat: position.coords.latitude + 0.02,
-              lng: position.coords.longitude + 0.02
+              lng: position.coords.longitude + 0++.02
             };
-            
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('You are here');
             infoWindow.open(map);
             map.setCenter(pos);
-
+            
+            //test code for a marker
             var marker = new google.maps.Marker({
               position: pos2,
               map: map,
