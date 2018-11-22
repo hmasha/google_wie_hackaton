2bd9cab043210a7d19484b6cc83883e82da53bea
mllemango <msmangochan@gmail.com>
2018-11-21 19:56:52 -0500

    Update get_display.php

--- get_display.php
+++ get_display.php
@@ -1,12 +1,12 @@
 <?php
 include 'connect.php';
-
+echo "test,i'm here";
 $sql = "SELECT * FROM goose";
 $result = pg_query($conn,$sql);
 if(!$result) { 
     $error= pg_last_error($conn);
     echo $error;
-    echo 'why oh please display';
+    echo "why oh please display";
 }
 else{
 while($row = pg_fetch_assoc($result)) {
