b5b81ad45a36a338c984ed03d7148029a339eef0
Melanie Wang <msmangochan@gmail.com>
2018-11-21 19:53:27 -0500

    reading in markers

--- get_display.php
+++ get_display.php
@@ -3,10 +3,20 @@
 $sql = "SELECT lat, long FROM goose";
 $result = pg_query($conn,$sql);
 while($row = pg_fetch_assoc($result)) {
-$lat= $row["lat"];
-$long=$row["long"];
+    $lat= $row["lat"];
+    $long=$row["long"];
+    ?>
 
+    <script>
+        var marker = new google.maps.Marker({
+              position: pos2,
+              map: map,
+              title: 'Hello World!'
+            });
+    </script>
+
+    <?php
+    
 }
-
 
 ?>
